93c460b25bf66e4dab162d6bb37108eb5a563b28
S <null>
2019-11-11 22:10:28 -0800

    Fix MySQL access Add ssl to config

--- src/main/java/net/urbanmc/ezauctions/datastorage/MySQLStorage.java
+++ src/main/java/net/urbanmc/ezauctions/datastorage/MySQLStorage.java
@@ -10,13 +10,11 @@ import org.bukkit.configuration.file.FileConfiguration;
 
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.logging.Level;
 
 public class MySQLStorage extends SQLStorage {
 
     private HikariDataSource hikariDS;
-    private String database;
 
     public MySQLStorage(EzAuctions plugin) {
         super(plugin);
@@ -29,44 +27,29 @@ public class MySQLStorage extends SQLStorage {
 
     @Override
     public boolean testAccess() {
-        return establishDB() && createDatabase(database) && createTables();
+        return establishDB() && createTables();
     }
 
     private boolean establishDB() {
         FileConfiguration config = ConfigManager.getConfig();
         String host = config.getString("data.mysql.host", "host");
-        database = config.getString("data.mysql.database");
-        String user = config.getString("data.mysql.user");
+        String database = config.getString("data.mysql.database");
+        String user = config.getString("data.mysql.username");
         String pass = config.getString("data.mysql.password");
         int port = config.getInt("data.mysql.port");
+        boolean ssl = config.getBoolean("data.mysql.useSSL", false);
 
         HikariConfig hikariConfig = new HikariConfig();
         hikariConfig.setPoolName("ezAuctionPool");
         hikariConfig.setUsername(user);
         hikariConfig.setPassword(pass);
-        hikariConfig.setJdbcUrl("jdbc:mysql://" + host + ":" + port + "/");
+        hikariConfig.setJdbcUrl("jdbc:mysql://" + host + ":" + port + "/" + database + "?useSSL=" + ssl);
 
         try {
             hikariDS = new HikariDataSource(hikariConfig);
         } catch (HikariPool.PoolInitializationException exception) {
             Bukkit.getLogger().severe("[ezAuctions] Error connecting to SQL Database. Please make sure everything is configured properly.");
             hikariDS = null;
-            return false;
-        }
-
-        return true;
-    }
-
-    private boolean createDatabase(String databaseName) {
-        try(Connection con = getConnection()) {
-            Statement statement = con.createStatement();
-
-            String sql = "CREATE DATABASE IF NOT EXISTS " + databaseName;
-            statement.execute(sql);
-
-            statement.close();
-        } catch (Exception e) {
-            Bukkit.getLogger().log(Level.SEVERE, "[ezAuctions] Could not connect to MySQL database!" + databaseName, e);
             return false;
         }
 

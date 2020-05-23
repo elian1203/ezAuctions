c1229eba559c7cb26dfaa919319fa1a735740e1c
S <null>
2020-05-22 23:31:42 -0700

    Use JUnit 5

--- src/test/java/net/urbanmc/ezauctions/TestRewardUtil.java
+++ src/test/java/net/urbanmc/ezauctions/TestRewardUtil.java
@@ -1,10 +1,10 @@
 package net.urbanmc.ezauctions;
 
 import org.bukkit.entity.Player;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import static org.mockito.Mockito.*;
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TestRewardUtil  {
 

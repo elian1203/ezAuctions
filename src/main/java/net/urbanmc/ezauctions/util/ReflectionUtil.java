6df443099d1fae22821acc61b7ae145231732607
Elian <elian1203@gmail.com>
2021-12-12 00:18:59 -0500

    Move away from NMS for finding repair cost and minecraft name. NMS still required for getting item json

--- src/main/java/net/urbanmc/ezauctions/util/ReflectionUtil.java
+++ src/main/java/net/urbanmc/ezauctions/util/ReflectionUtil.java
@@ -1,9 +1,15 @@
 package net.urbanmc.ezauctions.util;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import net.urbanmc.ezauctions.EzAuctions;
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.nio.charset.StandardCharsets;
 import java.util.logging.Level;
@@ -11,186 +17,189 @@ import java.util.logging.Level;
 @SuppressWarnings("ConstantConditions")
 public class ReflectionUtil {
 
-	private static Class<?> bannerClass;
-	private static double version;
+    private static Class<?> bannerClass;
+    private static double version;
 
-	// Put the version into a double
-	static {
-		try {
-			String version = Bukkit.getVersion(); // "git-Paper-153 MC: 1.13.2"
-			String[] spaceSplit = version.split(" "); // { "git-Paper-153", "MC:", "1.13.2" }
-			String numberVersion = spaceSplit[2].replace("(", "").replace(")", ""); // "1.13.2"
-			String[] dotSplit = numberVersion.split("\\."); // { "1", "13", "2" }
-			ReflectionUtil.version = Double.parseDouble(dotSplit[0] + "." + dotSplit[1]); // 1.13
+    // Put the version into a double
+    static {
+        try {
+            String version = Bukkit.getVersion(); // "git-Paper-153 MC: 1.13.2"
+            String[] spaceSplit = version.split(" "); // { "git-Paper-153", "MC:", "1.13.2" }
+            String numberVersion = spaceSplit[2].replace("(", "").replace(")", ""); // "1.13.2"
+            String[] dotSplit = numberVersion.split("\\."); // { "1", "13", "2" }
+            ReflectionUtil.version = Double.parseDouble(dotSplit[0] + "." + dotSplit[1]); // 1.13
 
-			bannerClass = getItemBannerClass();
-		} catch (Exception e) {
-			// In case anything goes wrong, assume it's a newer version
-			EzAuctions.getPluginLogger().severe("Could not determine Bukkit version for reflection. " +
-					"Assuming 1.18.");
-			version = 1.18;
-		}
-	}
+            bannerClass = getItemBannerClass();
+        } catch (Exception e) {
+            // In case anything goes wrong, assume it's a newer version
+            EzAuctions.getPluginLogger().severe("Could not determine Bukkit version for reflection. " +
+                    "Assuming 1.18.");
+            version = 1.18;
+        }
+    }
 
-	public static String getMinecraftName(ItemStack is) {
-		try {
-			Object nmsStack = asNMSCopy(is);
+    public static String getMinecraftName(ItemStack is) {
+        try {
+            Object nmsStack = asNMSCopy(is);
 
-			if (version >= 1.18) {
-				return NMSItemUtil.getItemLocalKey(nmsStack);
-			}
+            if (version >= 1.18) {
+                Material material = is.getType();
+                return (material.isBlock() ? "block": "item") + ".minecraft." + material.toString().toLowerCase();
+            }
 
-			Object item = nmsStack.getClass().getMethod("getItem").invoke(nmsStack);
+            Object item = nmsStack.getClass().getMethod("getItem").invoke(nmsStack);
 
-			// We can do a simple version check like this which should work for future versions
-			if (version > 1.12) {
-				return (String) item.getClass().getMethod("getName").invoke(item);
-			} else {
-				if (bannerClass.isAssignableFrom(item.getClass())) {
-					Object enumColor = item.getClass().getMethod("c", nmsStack.getClass()).invoke(item, nmsStack);
-					String color = enumColor.getClass().getMethod("d").invoke(enumColor).toString();
+            // We can do a simple version check like this which should work for future versions
+            if (version > 1.12) {
+                return (String) item.getClass().getMethod("getName").invoke(item);
+            } else {
+                if (bannerClass.isAssignableFrom(item.getClass())) {
+                    Object enumColor = item.getClass().getMethod("c", nmsStack.getClass()).invoke(item, nmsStack);
+                    String color = enumColor.getClass().getMethod("d").invoke(enumColor).toString();
 
-					return "item.banner." + color + ".name";
-				} else {
-					return item.getClass().getMethod("a", nmsStack.getClass()).invoke(item, nmsStack).toString() +
-							".name";
-				}
-			}
-		} catch (Exception ex) {
-			EzAuctions.getPluginLogger()
-					.log(Level.WARNING, "Error getting minecraft name for " + is.getType().toString(),
-							ex);
-			return "";
-		}
-	}
+                    return "item.banner." + color + ".name";
+                } else {
+                    return item.getClass().getMethod("a", nmsStack.getClass()).invoke(item, nmsStack).toString() +
+                            ".name";
+                }
+            }
+        } catch (Exception ex) {
+            EzAuctions.getPluginLogger()
+                    .log(Level.WARNING, "Error getting minecraft name for " + is.getType().toString(),
+                            ex);
+            return "";
+        }
+    }
 
-	/**
-	 * @return 0 if the item has never been repaired or -1 if it is no longer repairable.
-	 */
-	public static int getXPForRepair(ItemStack is) {
-		try {
-			Object nmsStack = asNMSCopy(is);
+    /**
+     * @return 0 if the item has never been repaired or -1 if it is no longer repairable.
+     */
+    public static int getXPForRepair(ItemStack is) {
+        try {
+            Object nmsStack = asNMSCopy(is);
 
-			if (version >= 1.18) {
-				return NMSItemUtil.getXPForRepair(nmsStack);
-			}
+            if (version >= 1.18) {
+                int cost = (int) is.getItemMeta().serialize().getOrDefault("repair-cost", 0);
+                boolean repairable = cost <= 40;
+                return repairable ? cost : -1;
+            }
 
-			boolean hasTag = (boolean) nmsStack.getClass().getMethod("hasTag").invoke(nmsStack);
+            boolean hasTag = (boolean) nmsStack.getClass().getMethod("hasTag").invoke(nmsStack);
 
-			if (!hasTag)
-				return 0;
+            if (!hasTag)
+                return 0;
 
-			Object tag = nmsStack.getClass().getMethod("getTag").invoke(nmsStack);
+            Object tag = nmsStack.getClass().getMethod("getTag").invoke(nmsStack);
 
-			boolean hasKey = (boolean) tag.getClass().getMethod("hasKey", String.class).invoke(tag, "RepairCost");
+            boolean hasKey = (boolean) tag.getClass().getMethod("hasKey", String.class).invoke(tag, "RepairCost");
 
-			if (!hasKey)
-				return 0;
+            if (!hasKey)
+                return 0;
 
-			int cost = (int) tag.getClass().getMethod("getInt", String.class).invoke(tag, "RepairCost");
+            int cost = (int) tag.getClass().getMethod("getInt", String.class).invoke(tag, "RepairCost");
 
-			boolean repairable = cost <= 40;
+            boolean repairable = cost <= 40;
 
-			if (repairable)
-				return cost;
-			else
-				return -1;
-		} catch (Exception ex) {
-			EzAuctions.getPluginLogger().log(Level.WARNING,
-					"Error getting xp needed for repair for " + is.getType().toString(),
-					ex);
-			return 0;
-		}
-	}
+            if (repairable)
+                return cost;
+            else
+                return -1;
+        } catch (Exception ex) {
+            EzAuctions.getPluginLogger().log(Level.WARNING,
+                    "Error getting xp needed for repair for " + is.getType().toString(),
+                    ex);
+            return 0;
+        }
+    }
 
-	public static String getItemAsJson(ItemStack is) {
-		try {
-			Object nmsStack = asNMSCopy(is);
+    public static String getItemAsJson(ItemStack is) {
+        try {
+            Object nmsStack = asNMSCopy(is);
 
-			if (version >= 1.18) {
-				return NMSItemUtil.getItemJson(nmsStack);
-			}
+            if (version >= 1.18) {
+                return NMSItemUtil.getItemJson(nmsStack);
+            }
 
-			Class<?> nbtTagCompoundClazz = getNBTTagCompoundClass();
-			Method saveMethod = nmsStack.getClass().getMethod("save", nbtTagCompoundClazz);
+            Class<?> nbtTagCompoundClazz = getNBTTagCompoundClass();
+            Method saveMethod = nmsStack.getClass().getMethod("save", nbtTagCompoundClazz);
 
-			Object nmsNbtTagCompoundObj = nbtTagCompoundClazz.newInstance();
-			Object jsonItem = saveMethod.invoke(nmsStack, nmsNbtTagCompoundObj);
+            Object nmsNbtTagCompoundObj = nbtTagCompoundClazz.newInstance();
+            Object jsonItem = saveMethod.invoke(nmsStack, nmsNbtTagCompoundObj);
 
-			String itemJson = jsonItem.toString();
+            String itemJson = jsonItem.toString();
 
-			// Prevent sending a packet that could be mishandled by bungeecord
-			if (itemJson.getBytes(StandardCharsets.UTF_8).length > Short.MAX_VALUE) {
-				EzAuctions.getPluginLogger().severe("An item that exceeded max packet length was attempted to be " +
-						"auctioned!");
-				return getItemAsJson(new ItemStack(is.getType(), 1));
-			}
+            // Prevent sending a packet that could be mishandled by bungeecord
+            if (itemJson.getBytes(StandardCharsets.UTF_8).length > Short.MAX_VALUE) {
+                EzAuctions.getPluginLogger().severe("An item that exceeded max packet length was attempted to be " +
+                        "auctioned!");
+                return getItemAsJson(new ItemStack(is.getType(), 1));
+            }
 
-			return itemJson;
-		} catch (Exception ex) {
-			EzAuctions.getPluginLogger().log(Level.WARNING,
-					"Error getting item as json. Item: " + is.getType().toString(),
-					ex);
-			return "";
-		}
-	}
+            return itemJson;
+        } catch (Exception ex) {
+            EzAuctions.getPluginLogger().log(Level.WARNING,
+                    "Error getting item as json. Item: " + is.getType().toString(),
+                    ex);
+            return "";
+        }
+    }
 
-	private static Object asNMSCopy(ItemStack is) {
-		try {
-			return getCraftItemStackClass().getMethod("asNMSCopy", ItemStack.class).invoke(null, is);
-		} catch (Exception ex) {
-			EzAuctions.getPluginLogger().log(Level.WARNING,
-					"Error getting item as NMS copy. Item: " + is.getType().toString(),
-					ex);
-			return null;
-		}
-	}
+    private static Object asNMSCopy(ItemStack is) {
+        try {
+            return getCraftItemStackClass().getMethod("asNMSCopy", ItemStack.class).invoke(null, is);
+        } catch (Exception ex) {
+            EzAuctions.getPluginLogger().log(Level.WARNING,
+                    "Error getting item as NMS copy. Item: " + is.getType().toString(),
+                    ex);
+            return null;
+        }
+    }
 
-	private static Class<?> getCraftItemStackClass() {
-		String version = Bukkit.getServer().getClass().getPackage().getName().split("\\.")[3];
+    private static Class<?> getCraftItemStackClass() {
+        String version = Bukkit.getServer().getClass().getPackage().getName().split("\\.")[3];
 
-		try {
-			return Class.forName("org.bukkit.craftbukkit." + version + ".inventory.CraftItemStack");
-		} catch (ClassNotFoundException ex) {
-			ex.printStackTrace();
-			return null;
-		}
-	}
+        try {
+            return Class.forName("org.bukkit.craftbukkit." + version + ".inventory.CraftItemStack");
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+    }
 
-	private static Class<?> getItemBannerClass() {
-		if (version >= 1.17) {
-			try {
-				return Class.forName("net.minecraft.world.item.ItemBanner");
-			} catch (ClassNotFoundException e) {
-				e.printStackTrace();
-				return null;
-			}
-		} else {
-			return getNMSClass("ItemBanner");
-		}
-	}
+    private static Class<?> getItemBannerClass() {
+        if (version >= 1.17) {
+            try {
+                return Class.forName("net.minecraft.world.item.ItemBanner");
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+                return null;
+            }
+        } else {
+            return getNMSClass("ItemBanner");
+        }
+    }
 
-	private static Class<?> getNBTTagCompoundClass() {
-		if (version >= 1.17) {
-			try {
-				return Class.forName("net.minecraft.nbt.NBTTagCompound");
-			} catch (ClassNotFoundException e) {
-				e.printStackTrace();
-				return null;
-			}
-		} else {
-			return getNMSClass("NBTTagCompound");
-		}
-	}
+    private static Class<?> getNBTTagCompoundClass() {
+        if (version >= 1.17) {
+            try {
+                return Class.forName("net.minecraft.nbt.NBTTagCompound");
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+                return null;
+            }
+        } else {
+            return getNMSClass("NBTTagCompound");
+        }
+    }
 
-	private static Class<?> getNMSClass(String name) {
-		String version = Bukkit.getServer().getClass().getPackage().getName().split("\\.")[3];
+    private static Class<?> getNMSClass(String name) {
+        String version = Bukkit.getServer().getClass().getPackage().getName().split("\\.")[3];
 
-		try {
-			return Class.forName("net.minecraft.server." + version + "." + name);
-		} catch (ClassNotFoundException ex) {
-			ex.printStackTrace();
-			return null;
-		}
-	}
+        try {
+            return Class.forName("net.minecraft.server." + version + "." + name);
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+    }
 }
